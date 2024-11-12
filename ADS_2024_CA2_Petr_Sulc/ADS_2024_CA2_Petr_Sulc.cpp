05be8ba96697099378e39067f744c3c6bce8361e
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-11-12 10:17:15 +0000

    Initial commit

---
+++ ADS_2024_CA2_Petr_Sulc/ADS_2024_CA2_Petr_Sulc.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ADS2024CA2PetrSulc
+{
+	TEST_CLASS(ADS2024CA2PetrSulc)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
