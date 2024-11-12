e4861c4584531158276e5ec5ca95a8901272385c
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-11-12 10:37:32 +0000

    :new: Creating new project
    - Added provided BinaryTree.h
    - Added provided BSTNode.h
    - Added Unit Testing Project

---
+++ ADS_2024_CA2_Petr_Sulc_Unit_Testing/ADS_2024_CA2_Petr_Sulc_Unit_Testing.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ADS2024CA2PetrSulcUnitTesting
+{
+	TEST_CLASS(ADS2024CA2PetrSulcUnitTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
